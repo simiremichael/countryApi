bc2c1a28aef4d335a6c518bcda39b2f4eb691cb3
Michael Simire <simiremichael@gmail.com>
2025-03-23 17:29:31 +0100

    updated cors

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const axios = require('axios');
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
