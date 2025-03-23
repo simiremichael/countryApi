5caf053ec45617a44db2f693df63db024cd45507
Michael Simire <simiremichael@gmail.com>
2025-03-23 17:39:58 +0100

    updated

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const axios = require('axios');
-// const cors = require('cors');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Enable CORS for cross-origin requests
-// app.use(cors());
+ app.use(cors(
+    {
+    origin: ["http://localhost:3000", "https://servix-app.netlify.app"], // Replace with your frontend's URL
+    methods: ['GET'], // Allowed HTTP methods
+  }
+ ));
 
 // Define routes to call the 4 APIs with countryCode parameter
 app.get('/detectCountry', async (req, res) => {
