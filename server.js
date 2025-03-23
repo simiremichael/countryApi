f830a467b152a668391433cc144b7f47bcfca943
Michael Simire <simiremichael@gmail.com>
2025-03-23 17:29:19 +0100

    updated

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Enable CORS for cross-origin requests
-app.use(cors());
+// app.use(cors());
 
 // Define routes to call the 4 APIs with countryCode parameter
 app.get('/detectCountry', async (req, res) => {
